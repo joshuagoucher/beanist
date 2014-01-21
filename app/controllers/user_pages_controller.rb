5a13bb26b386530cb90e057f599f4cdd9719553a
josh <joshuagoucher1@live.co.uk>
2014-01-21 11:42:24 +0000

    cleaning up responsiveness

--- app/controllers/user_pages_controller.rb
+++ app/controllers/user_pages_controller.rb
@@ -1,8 +1,8 @@
 class UserPagesController < ApplicationController
   def index
   	@feed = FeedEntry.last(12).reverse
-  	@featured = FeaturedArticle.todays_articles.find_with_reputation(:votes, :all, order: "votes desc")
-    @favourites = FeaturedArticle.last_week.find_with_reputation(:votes, :all, order: "votes desc")
+  	@featured = FeaturedArticle.todays_articles.find_with_reputation(:votes, :all, order: "votes desc").take(6)
+    @favourites = FeaturedArticle.last_week.find_with_reputation(:votes, :all, order: "votes desc").take(6)
   end
 
   
