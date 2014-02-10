08c30e1fdaa15b82ad2a811d1514868860ce4c61
josh <joshuagoucher1@live.co.uk>
2014-02-10 17:11:26 +0000

    added more data to seed.rb2

--- db/seeds.rb
+++ db/seeds.rb
@@ -115,3 +115,28 @@ FeaturedArticle.create(
 			pubdate: '20 january 2014',
 			source: 'jimseven.com'
 	)
+
+FeaturedArticle.create(
+			img_url: 'http://i.imgur.com/2HazJKL.png',
+			title:   "Has Bean Top Ten Coffees 2013 (Video)",
+			summary: "Every year I do a review of my top ten favourite coffees of the year
+					  Its a lonely thing so I decided to invite Roland (one of the roasters with us at Has Bean) 
+					  along to review the year with me. I hope you enjoy",
+			url:      'http://www.hasblog.co.uk/has-bean-top-ten-coffees-2013',
+			editors_comment: 'Hello Roland!',
+			pubdate: '1 january 2014',
+			source: 'hasblog.co.uk'
+	)
+
+FeaturedArticle.create(
+			img_url: 'http://i.imgur.com/Bd6ijxt.jpg',
+			title:   "Coffee Documentary “Yarasquin” to Screen in Subjects’ Home Village in Honduras",
+			summary: "In what promises to be an emotional celebration, filmmaker Sarah Gerber plans to screen her short coffee documentary, “The Way Back to Yarasquin,” to the subjects of the film: coffee workers and families in Santa Elena, Honduras.
+					  The film follows Mayra Orellana-Powell of green coffee importer Royal Coffee and Catracha Coffee 
+					  Co., on a journey with several other coffee professionals from the Bay Area to Mayra’s hometown of Santa Elena, where she works to connect the 
+                      farms to specialty coffee buyers and in turn helps improve the standard of living for farming families.",
+			url:      'http://dailycoffeenews.com/2014/01/22/coffee-documentary-yarasquin-to-screen-in-subjects-home-village-in-honduras/',
+			editors_comment: 'Cinema for the caffeine inclined',
+			pubdate: '22 january 2014',
+			source: 'dailycoffeenews.com'
+	)
