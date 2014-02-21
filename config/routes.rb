a76b4cd06bae63dbe44908404ebb6c3188fe3dde
josh <joshuagoucher1@live.co.uk>
2014-02-21 15:57:34 +0000

    fixed route 'author' bug

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Beanist2::Application.routes.draw do
 
   get "tags/:tag", to: 'featured_articles#index', as: :tag
 
-  get '/a/:author',    to: 'featured_articles#index', as: :author
+  get '/a/:tag',    to: 'featured_articles#index'
 
   get '/:id',      to: 'featured_articles#show'
 
