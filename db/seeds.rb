bcce99419f87ea24843c9630b95600e598603b9f
josh <joshuagoucher1@live.co.uk>
2014-02-24 16:45:07 +0000

    fixed posts for admin not showing

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,148 +8,148 @@
 
 
 
-# FeaturedArticle.create(
-# 			img_url: 'http://i.imgur.com/Bd6ijxt.jpg',
-# 			title:   "Coffee Documentary “Yarasquin” to Screen in Subjects’ Home Village in Honduras",
-# 			summary: "In what promises to be an emotional celebration, filmmaker Sarah Gerber plans to screen her short coffee documentary, “The Way Back to Yarasquin,” to the subjects of the film: coffee workers and families in Santa Elena, Honduras.
-# 					  The film follows Mayra Orellana-Powell of green coffee importer Royal Coffee and Catracha Coffee 
-# 					  Co., on a journey with several other coffee professionals from the Bay Area to Mayra’s hometown of Santa Elena, where she works to connect the 
-#                       farms to specialty coffee buyers and in turn helps improve the standard of living for farming families.",
-# 			url:      'http://dailycoffeenews.com/2014/01/22/coffee-documentary-yarasquin-to-screen-in-subjects-home-village-in-honduras/',
-# 			editors_comment: 'Cinema for the caffeine inclined',
-# 			pubdate: '22 january 2014',
-# 			source: 'dailycoffeenews.com',
-# 			tag_list: 'dailycoffeenews, news, coffee movie'
-# 	)
+FeaturedArticle.create(
+			img_url: 'http://i.imgur.com/Bd6ijxt.jpg',
+			title:   "Coffee Documentary “Yarasquin” to Screen in Subjects’ Home Village in Honduras",
+			summary: "In what promises to be an emotional celebration, filmmaker Sarah Gerber plans to screen her short coffee documentary, “The Way Back to Yarasquin,” to the subjects of the film: coffee workers and families in Santa Elena, Honduras.
+					  The film follows Mayra Orellana-Powell of green coffee importer Royal Coffee and Catracha Coffee 
+					  Co., on a journey with several other coffee professionals from the Bay Area to Mayra’s hometown of Santa Elena, where she works to connect the 
+                      farms to specialty coffee buyers and in turn helps improve the standard of living for farming families.",
+			url:      'http://dailycoffeenews.com/2014/01/22/coffee-documentary-yarasquin-to-screen-in-subjects-home-village-in-honduras/',
+			editors_comment: 'Cinema for the caffeine inclined',
+			pubdate: '22 january 2014',
+			source: 'dailycoffeenews.com',
+			tag_list: 'dailycoffeenews, news, coffee movie'
+	)
 
-# FeaturedArticle.create(
-# 			img_url: 'http://i.imgur.com/2HazJKL.png',
-# 			title:   "Has Bean Top Ten Coffees 2013 (Video)",
-# 			summary: "Every year I do a review of my top ten favourite coffees of the year
-# 					  Its a lonely thing so I decided to invite Roland (one of the roasters with us at Has Bean) 
-# 					  along to review the year with me. I hope you enjoy",
-# 			url:      'http://www.hasblog.co.uk/has-bean-top-ten-coffees-2013',
-# 			editors_comment: 'Hello Roland!',
-# 			pubdate: '1 january 2014',
-# 			source: 'hasblog.co.uk',
-# 			tag_list: 'hasblog, video, coffee reviews, steve leighton'
-# 	)
+FeaturedArticle.create(
+			img_url: 'http://i.imgur.com/2HazJKL.png',
+			title:   "Has Bean Top Ten Coffees 2013 (Video)",
+			summary: "Every year I do a review of my top ten favourite coffees of the year
+					  Its a lonely thing so I decided to invite Roland (one of the roasters with us at Has Bean) 
+					  along to review the year with me. I hope you enjoy",
+			url:      'http://www.hasblog.co.uk/has-bean-top-ten-coffees-2013',
+			editors_comment: 'Hello Roland!',
+			pubdate: '1 january 2014',
+			source: 'hasblog.co.uk',
+			tag_list: 'hasblog, video, coffee reviews, steve leighton'
+	)
 
-# FeaturedArticle.create(
-# 			img_url: 'http://i.imgur.com/ZCu8hEN.jpg',
-# 			title:   "An Introduction To Acidity",
-# 			summary: "Acidity is an important part of coffee, and weaves its way through the entire supply chain of the
-# 					  industry from variety to picking, from process to roasting and from brewing to tasting. I want to start 
-# 					  with the basics, cover some of the tasting aspect and then look at acids specific to coffee.
-# 					  We tend to define acids as having a pH lower than 7, or as having a sour taste, but these aren’t 
-# 					  particularly scientific. In fact there are three different scientific definitions of an acid: 
-# 					  The Arrhenius definition, the Brønsted-Lowry definition and the Lewis definition.",
-# 			url:      'http://www.jimseven.com/2014/01/20/introduction-acidity/',
-# 			editors_comment: 'When good flavours become good friends',
-# 			pubdate: '20 january 2014',
-# 			source: 'jimseven.com',
-# 			tag_list: 'jimseven, education, james hoffman, acidity'
-# 	)
+FeaturedArticle.create(
+			img_url: 'http://i.imgur.com/ZCu8hEN.jpg',
+			title:   "An Introduction To Acidity",
+			summary: "Acidity is an important part of coffee, and weaves its way through the entire supply chain of the
+					  industry from variety to picking, from process to roasting and from brewing to tasting. I want to start 
+					  with the basics, cover some of the tasting aspect and then look at acids specific to coffee.
+					  We tend to define acids as having a pH lower than 7, or as having a sour taste, but these aren’t 
+					  particularly scientific. In fact there are three different scientific definitions of an acid: 
+					  The Arrhenius definition, the Brønsted-Lowry definition and the Lewis definition.",
+			url:      'http://www.jimseven.com/2014/01/20/introduction-acidity/',
+			editors_comment: 'When good flavours become good friends',
+			pubdate: '20 january 2014',
+			source: 'jimseven.com',
+			tag_list: 'jimseven, education, james hoffman, acidity'
+	)
 
-# FeaturedArticle.create(
-# 			img_url: 'http://i.imgur.com/2HazJKL.png',
-# 			title:   "Has Bean Top Ten Coffees 2013 (Video)",
-# 			summary: "Every year I do a review of my top ten favourite coffees of the year
-# 					  Its a lonely thing so I decided to invite Roland (one of the roasters with us at Has Bean) 
-# 					  along to review the year with me. I hope you enjoy",
-# 			url:      'http://www.hasblog.co.uk/has-bean-top-ten-coffees-2013',
-# 			editors_comment: 'Hello Roland!',
-# 			pubdate: '1 january 2014',
-# 			source: 'hasblog.co.uk',
-# 			tag_list: 'hasblog, video, coffee reviews, steve leighton'
-# 	)
+FeaturedArticle.create(
+			img_url: 'http://i.imgur.com/2HazJKL.png',
+			title:   "Has Bean Top Ten Coffees 2013 (Video)",
+			summary: "Every year I do a review of my top ten favourite coffees of the year
+					  Its a lonely thing so I decided to invite Roland (one of the roasters with us at Has Bean) 
+					  along to review the year with me. I hope you enjoy",
+			url:      'http://www.hasblog.co.uk/has-bean-top-ten-coffees-2013',
+			editors_comment: 'Hello Roland!',
+			pubdate: '1 january 2014',
+			source: 'hasblog.co.uk',
+			tag_list: 'hasblog, video, coffee reviews, steve leighton'
+	)
 
-# FeaturedArticle.create(
-# 			img_url: 'http://i.imgur.com/ZCu8hEN.jpg',
-# 			title:   "An Introduction To Acidity",
-# 			summary: "Acidity is an important part of coffee, and weaves its way through the entire supply chain of the
-# 					  industry from variety to picking, from process to roasting and from brewing to tasting. I want to start 
-# 					  with the basics, cover some of the tasting aspect and then look at acids specific to coffee.
-# 					  We tend to define acids as having a pH lower than 7, or as having a sour taste, but these aren’t 
-# 					  particularly scientific. In fact there are three different scientific definitions of an acid: 
-# 					  The Arrhenius definition, the Brønsted-Lowry definition and the Lewis definition.",
-# 			url:      'http://www.jimseven.com/2014/01/20/introduction-acidity/',
-# 			editors_comment: 'When good flavours become good friends',
-# 			pubdate: '20 january 2014',
-# 			source: 'jimseven.com',
-# 			tag_list: 'jimseven, education, james hoffman, acidity'
-# 	)
+FeaturedArticle.create(
+			img_url: 'http://i.imgur.com/ZCu8hEN.jpg',
+			title:   "An Introduction To Acidity",
+			summary: "Acidity is an important part of coffee, and weaves its way through the entire supply chain of the
+					  industry from variety to picking, from process to roasting and from brewing to tasting. I want to start 
+					  with the basics, cover some of the tasting aspect and then look at acids specific to coffee.
+					  We tend to define acids as having a pH lower than 7, or as having a sour taste, but these aren’t 
+					  particularly scientific. In fact there are three different scientific definitions of an acid: 
+					  The Arrhenius definition, the Brønsted-Lowry definition and the Lewis definition.",
+			url:      'http://www.jimseven.com/2014/01/20/introduction-acidity/',
+			editors_comment: 'When good flavours become good friends',
+			pubdate: '20 january 2014',
+			source: 'jimseven.com',
+			tag_list: 'jimseven, education, james hoffman, acidity'
+	)
 
 
-# FeaturedArticle.create(
-# 			img_url: 'http://i.imgur.com/2HazJKL.png',
-# 			title:   "Has Bean Top Ten Coffees 2013 (Video)",
-# 			summary: "Every year I do a review of my top ten favourite coffees of the year
-# 					  Its a lonely thing so I decided to invite Roland (one of the roasters with us at Has Bean) 
-# 					  along to review the year with me. I hope you enjoy",
-# 			url:      'http://www.hasblog.co.uk/has-bean-top-ten-coffees-2013',
-# 			editors_comment: 'Hello Roland!',
-# 			pubdate: '1 january 2014',
-# 			source: 'hasblog.co.uk',
-# 			tag_list: 'hasblog, video, coffee reviews, steve leighton'
-# 	)
+FeaturedArticle.create(
+			img_url: 'http://i.imgur.com/2HazJKL.png',
+			title:   "Has Bean Top Ten Coffees 2013 (Video)",
+			summary: "Every year I do a review of my top ten favourite coffees of the year
+					  Its a lonely thing so I decided to invite Roland (one of the roasters with us at Has Bean) 
+					  along to review the year with me. I hope you enjoy",
+			url:      'http://www.hasblog.co.uk/has-bean-top-ten-coffees-2013',
+			editors_comment: 'Hello Roland!',
+			pubdate: '1 january 2014',
+			source: 'hasblog.co.uk',
+			tag_list: 'hasblog, video, coffee reviews, steve leighton'
+	)
 
-# FeaturedArticle.create(
-# 			img_url: 'http://i.imgur.com/Bd6ijxt.jpg',
-# 			title:   "Coffee Documentary “Yarasquin” to Screen in Subjects’ Home Village in Honduras",
-# 			summary: "In what promises to be an emotional celebration, filmmaker Sarah Gerber plans to screen her short coffee documentary, “The Way Back to Yarasquin,” to the subjects of the film: coffee workers and families in Santa Elena, Honduras.
-# 					  The film follows Mayra Orellana-Powell of green coffee importer Royal Coffee and Catracha Coffee 
-# 					  Co., on a journey with several other coffee professionals from the Bay Area to Mayra’s hometown of Santa Elena, where she works to connect the 
-#                       farms to specialty coffee buyers and in turn helps improve the standard of living for farming families.",
-# 			url:      'http://dailycoffeenews.com/2014/01/22/coffee-documentary-yarasquin-to-screen-in-subjects-home-village-in-honduras/',
-# 			editors_comment: 'Cinema for the caffeine inclined',
-# 			pubdate: '22 january 2014',
-# 			source: 'dailycoffeenews.com',
-# 			tag_list: 'dailycoffeenews, news, coffee movie'
-# 	)
+FeaturedArticle.create(
+			img_url: 'http://i.imgur.com/Bd6ijxt.jpg',
+			title:   "Coffee Documentary “Yarasquin” to Screen in Subjects’ Home Village in Honduras",
+			summary: "In what promises to be an emotional celebration, filmmaker Sarah Gerber plans to screen her short coffee documentary, “The Way Back to Yarasquin,” to the subjects of the film: coffee workers and families in Santa Elena, Honduras.
+					  The film follows Mayra Orellana-Powell of green coffee importer Royal Coffee and Catracha Coffee 
+					  Co., on a journey with several other coffee professionals from the Bay Area to Mayra’s hometown of Santa Elena, where she works to connect the 
+                      farms to specialty coffee buyers and in turn helps improve the standard of living for farming families.",
+			url:      'http://dailycoffeenews.com/2014/01/22/coffee-documentary-yarasquin-to-screen-in-subjects-home-village-in-honduras/',
+			editors_comment: 'Cinema for the caffeine inclined',
+			pubdate: '22 january 2014',
+			source: 'dailycoffeenews.com',
+			tag_list: 'dailycoffeenews, news, coffee movie'
+	)
 
-# FeaturedArticle.create(
-# 			img_url: 'http://i.imgur.com/ZCu8hEN.jpg',
-# 			title:   "An Introduction To Acidity",
-# 			summary: "Acidity is an important part of coffee, and weaves its way through the entire supply chain of the
-# 					  industry from variety to picking, from process to roasting and from brewing to tasting. I want to start 
-# 					  with the basics, cover some of the tasting aspect and then look at acids specific to coffee.
-# 					  We tend to define acids as having a pH lower than 7, or as having a sour taste, but these aren’t 
-# 					  particularly scientific. In fact there are three different scientific definitions of an acid: 
-# 					  The Arrhenius definition, the Brønsted-Lowry definition and the Lewis definition.",
-# 			url:      'http://www.jimseven.com/2014/01/20/introduction-acidity/',
-# 			editors_comment: 'When good flavours become good friends',
-# 			pubdate: '20 january 2014',
-# 			source: 'jimseven.com',
-# 			tag_list: 'jimseven, education, james hoffman, acidity'
-# 	)
+FeaturedArticle.create(
+			img_url: 'http://i.imgur.com/ZCu8hEN.jpg',
+			title:   "An Introduction To Acidity",
+			summary: "Acidity is an important part of coffee, and weaves its way through the entire supply chain of the
+					  industry from variety to picking, from process to roasting and from brewing to tasting. I want to start 
+					  with the basics, cover some of the tasting aspect and then look at acids specific to coffee.
+					  We tend to define acids as having a pH lower than 7, or as having a sour taste, but these aren’t 
+					  particularly scientific. In fact there are three different scientific definitions of an acid: 
+					  The Arrhenius definition, the Brønsted-Lowry definition and the Lewis definition.",
+			url:      'http://www.jimseven.com/2014/01/20/introduction-acidity/',
+			editors_comment: 'When good flavours become good friends',
+			pubdate: '20 january 2014',
+			source: 'jimseven.com',
+			tag_list: 'jimseven, education, james hoffman, acidity'
+	)
 
-# FeaturedArticle.create(
-# 			img_url: 'http://i.imgur.com/2HazJKL.png',
-# 			title:   "Has Bean Top Ten Coffees 2013 (Video)",
-# 			summary: "Every year I do a review of my top ten favourite coffees of the year
-# 					  Its a lonely thing so I decided to invite Roland (one of the roasters with us at Has Bean) 
-# 					  along to review the year with me. I hope you enjoy",
-# 			url:      'http://www.hasblog.co.uk/has-bean-top-ten-coffees-2013',
-# 			editors_comment: 'Hello Roland!',
-# 			pubdate: '1 january 2014',
-# 			source: 'hasblog.co.uk',
-# 			tag_list: 'hasblog, video, coffee reviews, steve leighton'
-# 	)
+FeaturedArticle.create(
+			img_url: 'http://i.imgur.com/2HazJKL.png',
+			title:   "Has Bean Top Ten Coffees 2013 (Video)",
+			summary: "Every year I do a review of my top ten favourite coffees of the year
+					  Its a lonely thing so I decided to invite Roland (one of the roasters with us at Has Bean) 
+					  along to review the year with me. I hope you enjoy",
+			url:      'http://www.hasblog.co.uk/has-bean-top-ten-coffees-2013',
+			editors_comment: 'Hello Roland!',
+			pubdate: '1 january 2014',
+			source: 'hasblog.co.uk',
+			tag_list: 'hasblog, video, coffee reviews, steve leighton'
+	)
 
-# FeaturedArticle.create(
-# 			img_url: 'http://i.imgur.com/Bd6ijxt.jpg',
-# 			title:   "Coffee Documentary “Yarasquin” to Screen in Subjects’ Home Village in Honduras",
-# 			summary: "In what promises to be an emotional celebration, filmmaker Sarah Gerber plans to screen her short coffee documentary, “The Way Back to Yarasquin,” to the subjects of the film: coffee workers and families in Santa Elena, Honduras.
-# 					  The film follows Mayra Orellana-Powell of green coffee importer Royal Coffee and Catracha Coffee 
-# 					  Co., on a journey with several other coffee professionals from the Bay Area to Mayra’s hometown of Santa Elena, where she works to connect the 
-#                       farms to specialty coffee buyers and in turn helps improve the standard of living for farming families.",
-# 			url:      'http://dailycoffeenews.com/2014/01/22/coffee-documentary-yarasquin-to-screen-in-subjects-home-village-in-honduras/',
-# 			editors_comment: 'Cinema for the caffeine inclined',
-# 			pubdate: '22 january 2014',
-# 			source: 'dailycoffeenews.com',
-# 			tag_list: 'dailycoffeenews, news, coffee movie'
-# 	)
+FeaturedArticle.create(
+			img_url: 'http://i.imgur.com/Bd6ijxt.jpg',
+			title:   "Coffee Documentary “Yarasquin” to Screen in Subjects’ Home Village in Honduras",
+			summary: "In what promises to be an emotional celebration, filmmaker Sarah Gerber plans to screen her short coffee documentary, “The Way Back to Yarasquin,” to the subjects of the film: coffee workers and families in Santa Elena, Honduras.
+					  The film follows Mayra Orellana-Powell of green coffee importer Royal Coffee and Catracha Coffee 
+					  Co., on a journey with several other coffee professionals from the Bay Area to Mayra’s hometown of Santa Elena, where she works to connect the 
+                      farms to specialty coffee buyers and in turn helps improve the standard of living for farming families.",
+			url:      'http://dailycoffeenews.com/2014/01/22/coffee-documentary-yarasquin-to-screen-in-subjects-home-village-in-honduras/',
+			editors_comment: 'Cinema for the caffeine inclined',
+			pubdate: '22 january 2014',
+			source: 'dailycoffeenews.com',
+			tag_list: 'dailycoffeenews, news, coffee movie'
+	)
 
 User.create(
 	name: "test_user",
