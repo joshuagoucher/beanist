c9c828b59aca45fc7e80e6d10587323f04ddd906
josh <joshuagoucher1@live.co.uk>
2014-02-26 11:11:06 +0000

    added 4th row to user pages index

--- app/controllers/user_pages_controller.rb
+++ app/controllers/user_pages_controller.rb
@@ -1,6 +1,6 @@
 class UserPagesController < ApplicationController
   def index
-  	@featured = FeaturedArticle.last(9).reverse
+  	@featured = FeaturedArticle.last(12).reverse
   	@daily_favs = FeaturedArticle.today.find_with_reputation(:votes, :all, order: "votes desc").take(5)
     @week_favs = FeaturedArticle.last_week.find_with_reputation(:votes, :all, order: "votes desc").take(10)
     @recent_users = User.last_week
